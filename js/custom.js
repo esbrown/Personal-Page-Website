83771ed7a5639952bad38b9acc37ec55eaadae59
esbrown <ebrown@Ethans-MacBook-Pro-2.local>
2016-08-24 11:16:41 -0400

    Site finished

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(e){ "use strict";
+	$("#github").click(function(e) {
+		$("#github-popup").fadeIn(300);
+		e.stopImmediatePropagation();
+	});
+	$("#headshot").click(function(e) {
+		if ($("#headshot").attr('src') == "images/ethan.jpg") {
+			$("#headshot").hide();
+			$("#headshot").attr('src', 'images/ethan-cartoon.png').fadeIn();
+		}
+		else {
+			$("#headshot").hide();
+			$("#headshot").attr('src', 'images/ethan.jpg').fadeIn();
+		}
+	});
+	$("#gmail").click(function(e) {
+		$("#gmail-popup").fadeIn(300);
+		e.stopImmediatePropagation();
+	});
+	$(".close-popup-button").click(function() {
+		$(".popup-window").fadeOut(300);
+	});
+	$(document).click(function (e) {
+    	if($(".popup-window").is(":visible") && !$(".popup-content").is(e.target)) {
+        	$(".popup-window").fadeOut(300);
+    	}
+	});
+});
