1635f433286af23e28f6076575762d32cc615d53
Ethan Brown <esbrown@stanford.edu>
2017-09-30 13:29:10 -0700

    updated to reflect summer project

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,25 @@
-$(document).ready(function(){ "use strict";
-	$("#github").click(function(e) {
-		$("#github-popup").fadeIn(300);
+$(document).ready(function() {
+	'use strict';
+	$('#github').click(function(e) {
+		$('#github-popup').fadeIn(300);
 		e.stopImmediatePropagation();
 	});
-	$("#headshot").click(function() {
-		if ($("#headshot").attr('src') === "images/ethan.jpg") {
-			$("#headshot").hide();
-			$("#headshot").attr('src', 'images/ethan-cartoon.png').fadeIn();
-		}
-		else {
-			$("#headshot").hide();
-			$("#headshot").attr('src', 'images/ethan.jpg').fadeIn();
-		}
-	});
-	$("#gmail").click(function(e) {
-		$("#gmail-popup").fadeIn(300);
+	$('#gmail').click(function(e) {
+		$('#gmail-popup').fadeIn(300);
 		e.stopImmediatePropagation();
 	});
-	$(".close-popup-button").click(function() {
-		$(".popup-window").fadeOut(300);
+	$('.close-popup-button').click(function() {
+		$('.popup-window').fadeOut(300);
 	});
-	$(document).click(function (e) {
-    	if($(".popup-window").is(":visible") && !$(".popup-content").is(e.target)) {
-        	$(".popup-window").fadeOut(300);
-    	}
+	$(document).click(function(e) {
+		if (
+			$('.popup-window').is(':visible') &&
+			!$('.popup-content').is(e.target)
+		) {
+			$('.popup-window').fadeOut(300);
+		}
 	});
 	$(function() {
-    	$('.box').matchHeight();
+		$('.box').matchHeight();
 	});
 });
